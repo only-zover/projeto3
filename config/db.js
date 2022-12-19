2d75026ac214c365976d8ee90191d16727395ee6
only-zover <j0seextremehd@outlook.com>
2022-12-19 01:02:24 -0300

    HAHAHA

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV == "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://zover:UzI9QukVlwSjXtjm@projeto3.idrgdfc.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://vercel-admin-user:UzI9QukVlwSjXtjm@cluster0.av6abin.mongodb.net/?retryWrites=true&w=majority",
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost:27017/mongo" };
